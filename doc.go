975b22b71e4b913df07f27f88524c652d3fb7645
Jonathan Sharman <jsharman@niche.com>
2022-03-16 16:15:48 -0500

    Tweak/reformat README and package docs

--- doc.go
+++ doc.go
@@ -6,16 +6,16 @@ updates to/from JSON.
 Motivation
 
 It's often useful to define data updates using JSON objects, where each
-key-value pair represents a field and its new value - using null to indicate
+key-value pair represents an update to a field - using null to indicate
 deletion. If a certain key is not present, the corresponding field is not
 modified. We want to define go structs corresponding to these updates, which
 need to be marshalled to/from JSON.
 
 If we were to use pointer fields with the omitempty JSON struct tag option for
-these structs, then fields explicitly set to nil to be removed would simply be
-absent from the marshalled JSON, i.e. unchanged. If we were to use pointer
-fields without omitempty, then nil fields would be present and null in the JSON
-output, i.e. removed.
+these structs, then fields explicitly set to nil to be removed would instead
+simply be absent from the marshalled JSON, i.e. unchanged. If we were to use
+pointer fields without omitempty, then nil fields would be present and null in
+the JSON output, i.e. removed.
 
 The Update and SliceUpdate types distinguish between no-op and removal updates,
 allowing them to correctly and seamlessly unmarshal themselves from JSON.
